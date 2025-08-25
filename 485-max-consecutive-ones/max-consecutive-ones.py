3a2ecb2e08efc96dd8345a4c4f2e248d15cc2b1c
Zain Mahmood <125191545+Turbo9910@users.noreply.github.com>
2025-08-25 14:25:04 +0500

    Time: 15 ms (73.19%) | Memory: 20.1 MB (89.70%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -3,21 +3,13 @@ class Solution:
         max_count = 0
         local_count = 0
 
-        if len(nums) == 1:
-            if nums[0] == 1:
-                return 1
-
-        if 1 not in nums:
-            return 0
-        
-
-        for i in range(len(nums)- 1):
-            if nums[i] == 1 and nums[i + 1] == 1:
+        for i in nums:
+            if i:
                 local_count+=1
-            if nums[i] == 0:
-                local_count = 0
-            if local_count + 1 > max_count:
-                max_count = local_count +1
+                if local_count > max_count:
+                    max_count = local_count
+            else:
+                local_count=0
         return max_count
-       
+      
         
