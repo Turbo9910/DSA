ca71542238c067b977a866f4d94b1858254d11ac
Zain Mahmood <125191545+Turbo9910@users.noreply.github.com>
2025-08-31 21:45:01 +0500

    Time: 7 ms (54.61%) | Memory: 17.9 MB (54.51%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_map = {}
+        t_map = {}
+
+        for c1, c2 in zip(s, t):
+            if c1 in s_map and s_map[c1] != c2:
+                return False
+            if c2 in t_map and t_map[c2] != c1:
+                return False
+
+   
+            s_map[c1] = c2
+            t_map[c2] = c1
+
+        return True
