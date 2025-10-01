21938fe90062210ccc19e4c119543e60d2eff2ed
Zain Mahmood <125191545+Turbo9910@users.noreply.github.com>
2025-10-01 23:37:35 +0500

    Time: 7 ms (77.81%) | Memory: 19.1 MB (47.44%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -2,14 +2,10 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         c_map = Counter(s)
         j = 0
-        r = [0] * len(s)
+        r = ""
         c_map = dict(sorted(c_map.items(), key=lambda item: item[1], reverse=True))
         for k,v in c_map.items():
-            while c_map[k] != 0:
-                r[j] = k
-                c_map[k] -=1
-                j +=1
-        r = "".join(map(str, r))
+            r += k*v
         return r
         
 
